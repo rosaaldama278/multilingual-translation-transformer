e221462cf43cf058456e922538efa1c14d0f61d1
Ting Aldama <tingaldama278@gmail.com>
2024-06-05 10:42:14 -0700

    update README

--- scripts/inference.py
+++ scripts/inference.py
@@ -2,7 +2,7 @@
 Author: Ting Aldama tingaldama278@gmail.com
 Date: 2024-05-22 08:06:01
 LastEditors: Ting Aldama tingaldama278@gmail.com
-LastEditTime: 2024-06-05 10:26:39
+LastEditTime: 2024-06-05 10:38:57
 FilePath: /multi-lang-translation/scripts/inference.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
